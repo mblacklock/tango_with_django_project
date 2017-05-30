60a0e455780a0c9b20671f048b86dcc40ff095d5
Matthew Blacklock <Matthew Blacklock>
2017-05-30 14:33:43 +0100

    Cookies and session data

--- rango/views.py
+++ rango/views.py
@@ -6,23 +6,53 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 
+from datetime import datetime
 
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
 # Create your views here.
 
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        #visits = 1
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
+
 def index(request):
     # get a list of the 5 top liked categories
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
+
     context = {'categories': category_list,
                'pages': page_list}
+    
+    visitor_cookie_handler(request)
+
+    context['visits'] = request.session['visits']
     
     return render(request, 'rango/index.html', context)
 
 def about_page(request):
-    return render(request, 'rango/about.html', )
+    visitor_cookie_handler(request)
+    context = {'visits': request.session['visits']}
+    return render(request, 'rango/about.html', context)
 
 def show_category(request, category_name_slug):
     # Gets all pages in category_name_slug and creates dict
